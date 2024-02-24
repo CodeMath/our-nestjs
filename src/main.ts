51daeb0e228949763e9ceba9efc9bb40fd727ec9
CodeMath <xyrho123@naver.com>
2024-02-24 23:05:08 +0900

    로그 사용부터 커스텀 로그 서비스/모듈까지

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { LoggerService } from './logger/logger.service';
+// import { MyLogger } from './custom-logger';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule , {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(LoggerService));
+  
   const config = new DocumentBuilder()
   .setTitle("Users API")
   .setDescription("This is Users REST API")
